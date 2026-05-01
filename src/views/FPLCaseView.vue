8672591a408c6bd3c42ebfe80319d128bb10078b
shady356 <heenriko@gmail.com>
2026-05-01 12:46:53 +0200

    feat: use lang ts

--- src/views/FPLCaseView.vue
+++ src/views/FPLCaseView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref, watch } from 'vue'
 import FPLColorSwatches from '@/components/FPLColorSwatches.vue'
 import BaseButtonIcon from '@/components/BaseButtonIcon.vue'
